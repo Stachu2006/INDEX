4e9065f95f57d89f225ee99274beea48966d5ffe
Stachu2006 <119595057+Stachu2006@users.noreply.github.com>
2022-12-08 11:20:50 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,2 +1,11 @@
+//alert('Hallo, allest gut');
+
+const name = prompt("Wie heisst du?");
+
+console.log("I am console your browser, hello");
+
 const myHeading = document.querySelector("h1")
 myHeading.textContent = "Schones Wien";
+
+//const first-akapit = document.querySelector(".first-akapit");
+//first-akapit.textContent = "Mein Wien"
